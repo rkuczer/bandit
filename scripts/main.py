359cf69c6fe144c0baa4481e7f92a1cc7bf0840d
Warren Clark <59663085+OClark23@users.noreply.github.com>
2023-04-03 14:41:59 -0400

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from bandit import bandit
-export PATH="/opt/hostedtoolcache/Python/3.7.16/x64/bin:$PATH"
+
 
 if __name__ == "__main__":
     bandit.main()
