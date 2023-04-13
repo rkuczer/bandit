74670d85c133d0ccf40877506a8b9cf11870299f
rkuczer <ryan-kooz@hotmail.com>
2023-04-13 12:31:17 -0400

    making formatting changes for test to pass format checks

--- tests/unit/core/test_assert_used.py
+++ tests/unit/core/test_assert_used.py
@@ -7,7 +7,7 @@ from bandit.plugins.asserts import gen_config
 class TestAssertUsed(unittest.TestCase):
     def test_assert_used(self):
         context = {"filename": "test_file.py"}
-
+        #testing the result from context of a test file
         result = assert_used(context, gen_config("assert_used"))
         self.assertIsNone(result)
 
