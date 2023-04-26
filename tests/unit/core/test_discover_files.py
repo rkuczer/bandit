f32b348c78cf8b2a0469843ecf3f6b6ee050952d
rkuczer <ryan-kooz@hotmail.com>
2023-04-26 12:23:44 -0400

    format changes for updated test for discovering a non existant file

--- tests/unit/core/test_discover_files.py
+++ tests/unit/core/test_discover_files.py
@@ -7,12 +7,12 @@ from bandit.core.manager import BanditManager
 class TestDiscoverFiles(unittest.TestCase):
     def setUp(self):
         self.b_conf = BanditConfig()
-        self.b_mgr = BanditManager(self.b_conf, 'non_aggressive')
+        self.b_mgr = BanditManager(self.b_conf, "non_aggressive")
 
     def test_nonexisting_target(self):
         with self.assertRaises(FileNotFoundError):
-            self.b_mgr.discover_files(['nonexistant_file.py'])
+            self.b_mgr.discover_files(["nonexistant_file.py"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
