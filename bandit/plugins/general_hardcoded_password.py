ce7c7a9338a62e7fc3c1da2f4dda34536aa74461
Omar1 <omarali.2000.16@gmail.com>
2023-04-26 16:53:54 -0400

    I modified RE_WORDS to include word boundary anchors \b around each word. This will ensure that the regular expression matches only complete words and not parts of words.

--- bandit/plugins/general_hardcoded_password.py
+++ bandit/plugins/general_hardcoded_password.py
@@ -9,11 +9,10 @@ import bandit
 from bandit.core import issue
 from bandit.core import test_properties as test
 
-RE_WORDS = "(pas+wo?r?d|pass(phrase)?|pwd|secrete|password|pass|to+k?en|conn?)"
+RE_WORDS = r"\b(pas+wo?r?d|pass(phrase)?|pwd|secrete|password|pass|to+k?en|conn?)\b"
 RE_CANDIDATES = re.compile(
     "(^{0}$|_{0}_|^{0}_|_{0}$)".format(RE_WORDS), re.IGNORECASE
 )
-
 
 
 def _report(value):
@@ -35,29 +34,29 @@ def hardcoded_password_string(context):
             if isinstance(targ, ast.Name) and RE_CANDIDATES.search(targ.id):
                 return _report(node.s)
             elif isinstance(targ, ast.Attribute) and RE_CANDIDATES.search(
-                targ.attr
+                    targ.attr
             ):
                 return _report(node.s)
 
     elif isinstance(
-        node._bandit_parent, ast.Subscript
+            node._bandit_parent, ast.Subscript
     ) and RE_CANDIDATES.search(node.s):
         # Py39+: looks for "dict[candidate]='some_string'"
         # subscript -> index -> string
         assign = node._bandit_parent._bandit_parent
         if isinstance(assign, ast.Assign) and isinstance(
-            assign.value, ast.Str
+                assign.value, ast.Str
         ):
             return _report(assign.value.s)
 
     elif isinstance(node._bandit_parent, ast.Index) and RE_CANDIDATES.search(
-        node.s
+            node.s
     ):
         # looks for "dict[candidate]='some_string'"
         # assign -> subscript -> index -> string
         assign = node._bandit_parent._bandit_parent._bandit_parent
         if isinstance(assign, ast.Assign) and isinstance(
-            assign.value, ast.Str
+                assign.value, ast.Str
         ):
             return _report(assign.value.s)
 
@@ -77,7 +76,6 @@ def hardcoded_password_string(context):
 @test.checks("Call")
 @test.test_id("B106")
 def hardcoded_password_funcarg(context):
-
     # looks for "function(candidate='some_string')"
     for kw in context.node.keywords:
         if isinstance(kw.value, ast.Str) and RE_CANDIDATES.search(kw.arg):
@@ -87,12 +85,11 @@ def hardcoded_password_funcarg(context):
 @test.checks("FunctionDef")
 @test.test_id("B107")
 def hardcoded_password_default(context):
-
     # looks for "def function(candidate='some_string')"
 
     # this pads the list of default values with "None" if nothing is given
     defs = [None] * (
-        len(context.node.args.args) - len(context.node.args.defaults)
+            len(context.node.args.args) - len(context.node.args.defaults)
     )
     defs.extend(context.node.args.defaults)
 
