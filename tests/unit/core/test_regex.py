ef8a28debf403d7ea0c1c6e518059bd174e7239b
rkuczer <ryan-kooz@hotmail.com>
2023-04-28 14:20:59 -0400

    testing format changes for omar test

--- tests/unit/core/test_regex.py
+++ tests/unit/core/test_regex.py
@@ -4,6 +4,7 @@ import unittest
 from bandit.plugins.general_hardcoded_password import RE_CANDIDATES
 from bandit.plugins.general_hardcoded_password import RE_WORDS
 
+
 class TestRegex(unittest.TestCase):
     def test_valid_input(self):
         regex = re.compile(RE_CANDIDATES)
@@ -14,7 +15,7 @@ class TestRegex(unittest.TestCase):
             "secrete",
             "pass",
             "token",
-            "conn"
+            "conn",
         ]
         for input_str in valid_inputs:
             with self.subTest(input_str=input_str):
@@ -23,9 +24,28 @@ class TestRegex(unittest.TestCase):
 
 class MyTestCase(unittest.TestCase):
     def test_complete_words(self):
-        test_strings = ['passwords', 'secretpassword', 'pword', 'secret', 'pas', 'token', 'connection',
-                        'my password is secret', 'secrete']
-        expected_result = ['password', 'secrete', 'pwd', 'secret', 'pass', 'token', 'conn', 'password', 'secrete']
+        test_strings = [
+            'passwords',
+            'secretpassword',
+            'pword',
+            'secret',
+            'pas',
+            'token',
+            'connection',
+            'my password is secret',
+            'secrete'
+        ]
+        expected_result = [
+            'password',
+            'secrete',
+            'pwd',
+            'secret',
+            'pass',
+            'token',
+            'conn',
+            'password',
+            'secrete'
+        ]
 
         for index, string in enumerate(test_strings):
             match = re.search(RE_CANDIDATES, string)
@@ -33,7 +53,14 @@ class MyTestCase(unittest.TestCase):
                 assert match.group(1).lower() == expected_result[index]
 
     def test_false_positives(self):
-        test_strings = ['passenger', 'compass', 'passport', 'complicated', 'passive', 'passworded']
+        test_strings = [
+            'passenger',
+            'compass',
+            'passport',
+            'complicated',
+            'passive',
+            'passworded'
+        ]
 
         for string in test_strings:
             match = re.search(RE_CANDIDATES, string)
@@ -41,15 +68,15 @@ class MyTestCase(unittest.TestCase):
 
     def test_RE_WORDS(self):
         # Test with a word that contains 'password' as a substring
-        assert not re.search(RE_WORDS, 'notmypassword')
+        assert not re.search(RE_WORDS, "notmypassword")
         # Test with a word that contains 'pass' as a substring
-        assert not re.search(RE_WORDS, 'passenger')
+        assert not re.search(RE_WORDS, "passenger")
         # Test with a complete word 'password'
-        assert re.search(RE_WORDS, 'password')
+        assert re.search(RE_WORDS, "password")
         # Test with a complete word 'secrete'
-        assert re.search(RE_WORDS, 'secrete')
+        assert re.search(RE_WORDS, "secrete")
         # Test with a complete word 'token'
-        assert re.search(RE_WORDS, 'token')
+        assert re.search(RE_WORDS, "token")
 
 
 if __name__ == "__main__":
