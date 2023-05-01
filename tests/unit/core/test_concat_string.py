b72192db236d8a3d7a6f0824b687512e334c7fc9
rkuczer <ryan-kooz@hotmail.com>
2023-04-30 21:15:07 -0400

    adding in warrens test to make sure it's compatable with github actions

---
+++ tests/unit/core/test_concat_string.py
@@ -0,0 +1,34 @@
+import ast
+import unittest
+
+
+def extract_strings(node):
+    class StringVisitor(ast.NodeVisitor):
+        def __init__(self):
+            self.strings = []
+
+        def visit_Str(self, node):
+            self.strings.append(node.s)
+
+    visitor = StringVisitor()
+    visitor.visit(node)
+    result = (node, " ".join(visitor.strings))
+    return result
+
+
+class TestExtractStrings(unittest.TestCase):
+
+    def test_extract_strings(self):
+        # Create a simple AST node with two string literals
+        node = ast.parse("print('john,')\nprint('santore!')")
+        result = extract_strings(node)
+        # Verify that the result tuple contains the original node object
+        self.assertEqual(result[0], node)
+        # Verify that the result tuple contains a string with both string literals
+        self.assertEqual(result[1], "john, santore!")
+
+
+if __name__ == '__main__':
+    unittest.main()
+# Test for Warren's individual issue where the
+# concat strings function adds a space that should not be there
