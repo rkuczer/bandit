2ec4610311e64db305e80f03bcff61526d3595ca
rkuczer <ryan-kooz@hotmail.com>
2023-05-01 19:27:26 -0400

    format changes to test_functional since we are removing 3.7 support there is no longer a check needed for versions before 3.8

---
+++ tests/unit/core/test_python_version.py
@@ -0,0 +1,15 @@
+import ast
+import unittest
+
+from bandit.core.utils import linerange
+
+
+class TestLineRange(unittest.TestCase):
+    def test_linerange(self):
+        content = "def func():\n    print('hello world')\n\nx = 5\nif x > 3:\n    print('x is greater than 3')\n"
+        node = ast.parse(content)
+        self.assertEqual(linerange(node), [0, 1])
+
+
+if __name__ == "__main__":
+    unittest.main()
